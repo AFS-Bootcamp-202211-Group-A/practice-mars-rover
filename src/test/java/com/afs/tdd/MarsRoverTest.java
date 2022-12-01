1380327e1d7de13de64d3dee1be7167d2edbbcea
Hezz <heinrich@connect.hku.hk>
2022-12-01 17:07:54 +0800

    test: prepare test for should_0_0_E_when_R_given_0_0_N

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -2,9 +2,24 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
-class MarsRoverTest {
-    @Test
-    void write_your_first_test() {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class MarsRoverTest {
+
+    @Test
+    void should_0_0_E_when_R_given_0_0_N() {
+        //given
+        MarsRover rover = new MarsRover(0,0, "N");
+        
+        //when
+        rover.executeCommand("R");
+
+        //then
+        assertEquals(0, rover.getLocationX());
+        assertEquals(0, rover.getLocationY());
+        assertEquals("E", rover.getDirection());
     }
+
+
+
 }
