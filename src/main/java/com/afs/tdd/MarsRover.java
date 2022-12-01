8eea9a2053aad37a8a77121ecbb9e659feaa858c
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-01 18:07:52 +0800

    test: add test should_go_to_0_1_N_when_execute_batch_command_given_0_0_N_M

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -25,6 +25,10 @@ public class MarsRover {
         }
     }
 
+    public void executeBatchCommands(String batchCommand){
+
+    }
+
     private void turnRight(){
         switch (direction){
             case "N":
--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -158,4 +158,17 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getLocationY());
         assertEquals("N", marsRover.getDirection());
     }
+
+    @Test
+    void should_go_to_0_1_N_when_execute_batch_command_given_0_0_N_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+        //when
+        marsRover.executeBatchCommands("M");
+
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("N", marsRover.getDirection());
+    }
 }
