ecc05db0c96a348a50365ccc78945d3ebed61a85
Alan Wan <alan.wan@oocl.com>
2022-12-01 17:45:11 +0800

    feat: passed 4 cases about moving

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,13 +1,23 @@
 package com.afs.tdd;
 
+import javafx.geometry.Pos;
+
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class MarsRover {
+
+    public Position executeBatchCommand(Position position, String batchCommand){
+        return null;
+    }
 
     public Position executeCommand(Position position, String command) {
 
         if (command == "M") {
             return move(position);
+        }
+        else{
+
         }
         return null;
     }
@@ -19,7 +29,6 @@ public class MarsRover {
         } else if (direction == "E" || direction == "W") {
             position.setX(direction.equals("E") ? position.getX() + 1 : position.getX() - 1);
         }
-
         return position;
     }
 
--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class MarsRoverTest {
+class MarsRoverTest{
     @Test
     public void givenPosition00NCommandM_whenExecuteCommand_thenReturn01N() {
         //given
@@ -17,6 +17,47 @@ class MarsRoverTest {
         //then
         assertThat(actual).usingRecursiveComparison().isEqualTo(new Position(0, 1, "N"));
     }
+
+    @Test
+    public void givenPosition00ECommandM_whenExecuteCommand_thenReturn10E() {
+        //given
+        Position position = new Position(0, 0, "E");
+
+        //when
+        MarsRover car = new MarsRover();
+        Position actual = car.executeCommand(position, "M");
+
+        //then
+        assertThat(actual).usingRecursiveComparison().isEqualTo(new Position(1, 0, "E"));
+    }
+
+    @Test
+    public void givenPosition00SCommandM_whenExecuteCommand_thenReturn0Minus1S() {
+        //given
+        Position position = new Position(0, 0, "S");
+
+        //when
+        MarsRover car = new MarsRover();
+        Position actual = car.executeCommand(position, "M");
+
+        //then
+        assertThat(actual).usingRecursiveComparison().isEqualTo(new Position(0, -1, "S"));
+    }
+
+    @Test
+    public void givenPosition00WCommandM_whenExecuteCommand_thenReturnMinus10W() {
+        //given
+        Position position = new Position(0, 0, "W");
+
+        //when
+        MarsRover car = new MarsRover();
+        Position actual = car.executeCommand(position, "M");
+
+        //then
+        assertThat(actual).usingRecursiveComparison().isEqualTo(new Position(-1, 0, "W"));
+    }
+
+
 
 
 }
