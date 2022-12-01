8f470d041b494d2eef148d78fb3b76b01e78ef01
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-01 18:04:58 +0800

    feat: turns to N when facing E and executing L

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -55,6 +55,8 @@ public class MarsRover {
             case "S":
                 direction = "E";
                 break;
+            case "E":
+                direction = "N";
             default:
         }
     }
