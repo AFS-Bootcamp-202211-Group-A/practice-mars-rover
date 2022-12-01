b6157f563b09503df8954d070f8f936f3d93aad1
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-12-01 17:39:15 +0800

    test: add test should_go_to_0_0_N_when_execute_command_given_0_0_W_R

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -139,4 +139,16 @@ class MarsRoverTest {
         assertEquals(0,marsRover.getLocationY());
         assertEquals("S",marsRover.getDirection());
     }
+    @Test
+    void should_go_to_0_0_N_when_execute_command_given_0_0_W_R() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"W");
+        //when
+
+        marsRover.executeCommand("R");
+        //then
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(0,marsRover.getLocationY());
+        assertEquals("N",marsRover.getDirection());
+    }
 }
