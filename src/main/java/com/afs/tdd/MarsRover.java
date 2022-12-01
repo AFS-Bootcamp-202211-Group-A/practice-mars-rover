b829fd36c9238a734adcf7db62fbe0c76be401bd
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-12-01 17:04:18 +0800

    feat: execute command M when direction N

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,21 +1,37 @@
 package com.afs.tdd;
 
 public class MarsRover {
+
+    private int locationX;
+    private int locationY;
+    private String direction;
+
     public MarsRover(int locationX, int locationY, String direction) {
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.direction = direction;
     }
     public void executeCommand(String command){
+        if (command.equals("M")){
+            move();
+        }
+    }
 
+    private void move() {
+        if (direction.equals("N")){
+            locationY++;
+        }
     }
 
     public int getLocationX() {
-        return 0;
+        return locationX;
     }
 
     public int getLocationY() {
-        return 0;
+        return locationY;
     }
 
     public String getDirection() {
-        return "0";
+        return direction;
     }
 }
