630fbf177f76a26bdbc09f70d56b993b38fd3958
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-01 16:59:48 +0800

    test: add test should_go_to_0_1_N_when_execute_command_given_0_0_N_M

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,4 +1,25 @@
 package com.afs.tdd;
 
 public class MarsRover {
+    private int locationX;
+    private int locationY;
+    private String direction;
+    public MarsRover(int locationX, int locationY  , String direction) {
+    }
+
+    public void executeCommand(String command) {
+
+    }
+
+    public int getLocationX() {
+        return 0;
+    }
+
+    public int getLocationY() {
+        return 0;
+    }
+
+    public String getDirection() {
+        return null;
+    }
 }
--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -1,10 +1,20 @@
 package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarsRoverTest {
+    //0 0 N M -> 0 1 N
     @Test
-    void write_your_first_test() {
+    void should_go_to_0_1_N_when_execute_command_given_0_0_N_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        //when
+        marsRover.executeCommand("M");
 
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(1, marsRover.getLocationY());
+        assertEquals("N", marsRover.getDirection());
     }
 }
