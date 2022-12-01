3fc8552ed52c489646970cbe9b7cc601c2bb7a15
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-01 17:57:27 +0800

    feat: turns to N when facing W and executing R

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -33,6 +33,9 @@ public class MarsRover {
             case "S":
                 direction = "W";
                 break;
+            case "W":
+                direction = "N";
+                break;
             default:
 
         }
