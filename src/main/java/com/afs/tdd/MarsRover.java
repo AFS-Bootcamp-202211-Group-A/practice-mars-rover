1b44ccc59c045461084ef54c276eee1958d3a283
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-12-01 17:13:05 +0800

    feat: execute command M when direction W

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -24,6 +24,8 @@ public class MarsRover {
             locationY--;
         }else if (direction.equals("E")){
             locationX++;
+        }else if (direction.equals("W")){
+            locationX--;
         }
     }
 
