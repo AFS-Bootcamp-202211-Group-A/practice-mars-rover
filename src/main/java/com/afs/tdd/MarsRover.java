24615d6c400f9b68ed42c26a954b73e6533050d8
Alan Wan <alan.wan@oocl.com>
2022-12-01 17:22:50 +0800

    feat: passed the first test cases

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,7 +1,26 @@
 package com.afs.tdd;
 
+import java.util.HashMap;
+
 public class MarsRover {
-    public Position executeCommand(Position position, String command){
+
+    public Position executeCommand(Position position, String command) {
+
+        if (command == "M") {
+            return move(position);
+        }
         return null;
     }
+
+    private Position move(Position position) {
+        String direction = position.getDirection();
+        if (direction == "N" || direction == "S") {
+            position.setY(direction.equals("N") ? position.getY() + 1 : position.getY() - 1);
+        } else if (direction == "E" || direction == "W") {
+            position.setX(direction.equals("E") ? position.getX() + 1 : position.getX() - 1);
+        }
+
+        return position;
+    }
+
 }
