b108c2e77f16aaa757754aba27377d32f948e135
kwokth4 <kwokth4@oocl.com>
2022-12-01 17:11:18 +0800

    feat: should_go_to_0_1_N_when_execute_command_given_0_0_N_M Done

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -2,9 +2,16 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MarsRoverTest {
     @Test
-    void write_your_first_test() {
-
+    // 0 0 N M -> 0 1 N
+    void should_go_to_0_1_N_when_execute_command_given_0_0_N_M() {
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        marsRover.executeCommand("M");
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(1,marsRover.getLocationY());
+        assertEquals("N",marsRover.getDirection());
     }
 }
