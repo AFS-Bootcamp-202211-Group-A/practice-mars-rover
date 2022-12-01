0c85a1f99c68114564ecffe2ac05bda7bf964065
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-12-01 17:16:51 +0800

    refractor: refractored the string to constant

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -2,6 +2,11 @@ package com.afs.tdd;
 
 public class MarsRover {
 
+    public static final String N = "N";
+    public static final String S = "S";
+    public static final String E = "E";
+    public static final String W = "W";
+    public static final String M = "M";
     private int locationX;
     private int locationY;
     private String direction;
@@ -12,19 +17,19 @@ public class MarsRover {
         this.direction = direction;
     }
     public void executeCommand(String command){
-        if (command.equals("M")){
+        if (command.equals(M)){
             move();
         }
     }
 
     private void move() {
-        if (direction.equals("N")){
+        if (direction.equals(N)){
             locationY++;
-        } else if (direction.equals("S")){
+        } else if (direction.equals(S)){
             locationY--;
-        }else if (direction.equals("E")){
+        }else if (direction.equals(E)){
             locationX++;
-        }else if (direction.equals("W")){
+        }else if (direction.equals(W)){
             locationX--;
         }
     }
