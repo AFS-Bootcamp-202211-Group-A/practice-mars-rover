c101296478449250d7f4ac3b19b79f622309f040
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-12-01 17:06:37 +0800

    test: add test should_go_to_0_n1_S_when_execute_command_given_0_0_S_M

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarsRoverTest {
     @Test
-    void should_go_to_0_0_N_when_execute_command_given_0_0_N_M() {
+    void should_go_to_0_1_N_when_execute_command_given_0_0_N_M() {
         //given
         MarsRover marsRover = new MarsRover(0,0,"N");
         //when
@@ -16,6 +16,18 @@ class MarsRoverTest {
         assertEquals(0,marsRover.getLocationX());
         assertEquals(1,marsRover.getLocationY());
         assertEquals("N",marsRover.getDirection());
+    }
+    @Test
+    void should_go_to_0_n1_S_when_execute_command_given_0_0_S_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        //when
+
+        marsRover.executeCommand("M");
+        //then
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(-1,marsRover.getLocationY());
+        assertEquals("S",marsRover.getDirection());
     }
 
 }
