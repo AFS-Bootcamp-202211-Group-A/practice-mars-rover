fb3e3e4c2fbc8938c3a60864bac7a48038656ace
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-01 18:04:13 +0800

    test: add test should_go_to_0_0_N_when_execute_command_given_0_0_E_L

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -145,4 +145,17 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getLocationY());
         assertEquals("E", marsRover.getDirection());
     }
+
+    @Test
+    void should_go_to_0_0_N_when_execute_command_given_0_0_E_L() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+        //when
+        marsRover.executeCommand("L");
+
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("N", marsRover.getDirection());
+    }
 }
